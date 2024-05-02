4e5374b812e047926c7492cfe593a2baaa3d26b5
edeleon4490 <edeleon9@students.avc.edu>
2024-05-02 04:09:32 +0000

    Updated Amounts

--- BrLw.R
+++ BrLw.R
@@ -1,19 +1,15 @@
-BrLw <-function(numClass,odds,countoDo){
-
-days=1:365
-count=countoDo
-repeatn=numClass
-varstor=numeric()
-numWins=0
+BrLw <-function(numClass = 35,odds = 2,countoDo = 637){
+  
+  days=1:365
+  count=countoDo
+  repeatn=numClass
+  varstor=numeric()
   brownlow = 0
   for (i in 1:countoDo) {
     sam=sample(days,size=numClass,replace = T)
     y=duplicated(sam)
-    if (sum(y)>0) {
-      brownlow=brownlow+1
-    }
+    if (sum(y)>0) brownlow=brownlow+1
     else brownlow = brownlow - odds
-    
   }
- return(brownlow)
+  return(brownlow)
 }
